d2ab5696391668ea4b7aa4fde5591260006ee99e
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-04-11 17:53:46 +0530

    route

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url === "/") {
+    res.write("welcome to node server");
+  }
+  if (url === "/home") {
+    res.write("welcome to home");
+  }
+  if (url === "/about") {
+    res.write("welcome to about page");
+  }
+  if (url === "/node") {
+    res.write("welcome to my node js project");
+  }
+  res.end();
 });
 
 server.listen(3000);

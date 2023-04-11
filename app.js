8e224664b516cd5b84a7eb176a86119237d41d66
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-04-11 19:56:08 +0530

    reading file

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
+const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url === "/") {
-    res.write("welcome to node server");
+  const method = req.method;
+  fs.readFile("message.txt", (err, data) => {
+    if (url === "/") {
+      res.write("<html>");
+      res.write("<head><title>server</title><head>");
+      res.write(`<head><h1>${data}</h1><head>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"></input><button>send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    }
+  });
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message);
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
-  if (url === "/home") {
-    res.write("welcome to home");
-  }
-  if (url === "/about") {
-    res.write("welcome to about page");
-  }
-  if (url === "/node") {
-    res.write("welcome to my node js project");
-  }
-  res.end();
 });
 
 server.listen(3000);

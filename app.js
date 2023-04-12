4f5b982aa7e65dbc41f7ba98fa736b7b3bb48855
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-04-12 16:50:05 +0530

    server using express

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
-const fs = require("fs");
 const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  fs.readFile("message.txt", (err, data) => {
-    if (url === "/") {
-      res.write("<html>");
-      res.write("<head><title>server</title><head>");
-      res.write(`<head><h1>${data}</h1><head>`);
-      res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="message"></input><button>send</button></form></body>'
-      );
-      res.write("</html>");
-      return res.end();
-    }
-  });
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      console.log(message);
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
+app.use((req, res, next) => {
+  console.log("Running the middle ware!");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Running another middle ware!");
+  res.send({ key1: "value" });
 });
 
-server.listen(3000);
+app.listen(3000);

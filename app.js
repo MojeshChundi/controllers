0330f332288cf1e13e35b8c525ac3138f42b42fe
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-04-13 18:43:50 +0530

    static files

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 
+const adminRoutes = require("./routes/Admin");
+const shopRoutes = require("./routes/Shop");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use("/product-page", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'/><input type='text' name='title'/><button type='submit'>add product</button></form>"
-  );
-});
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello from node js!</h1>");
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
